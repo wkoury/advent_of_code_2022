7f2f72e69519c8009c1b1a905f5645255add3b17
Will Koury <wkoury@hey.com>
2022-12-03 17:39:43 -0500

    Day 4 part 1

---
+++ day3/src/main.rs
@@ -0,0 +1,97 @@
+#![warn(clippy::all)]
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    // Open the input file
+    let path: &Path = Path::new("./input.txt");
+    let display = path.display();
+    let mut file: File = match File::open(path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut contents = String::new();
+    if let Err(why) = file.read_to_string(&mut contents) {
+        panic!("couldn't read {}: {}", display, why);
+    }
+
+    let mut sum: u32 = 0;
+
+    for line in contents.lines() {
+        // Split the line into two halves
+        let (left, right) = line.split_at(line.len() / 2);
+
+        let mut similar_char: char = '0';
+        for left_char in left.chars() {
+            for right_char in right.chars() {
+                if left_char == right_char {
+                    similar_char = left_char;
+                }
+            }
+        }
+        sum += get_priority_from_char(similar_char);
+    }
+
+    println!("The sum of the priorities is: {}", sum);
+}
+
+fn get_priority_from_char(c: char) -> u32 {
+    match c {
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
+        'd' => 4,
+        'e' => 5,
+        'f' => 6,
+        'g' => 7,
+        'h' => 8,
+        'i' => 9,
+        'j' => 10,
+        'k' => 11,
+        'l' => 12,
+        'm' => 13,
+        'n' => 14,
+        'o' => 15,
+        'p' => 16,
+        'q' => 17,
+        'r' => 18,
+        's' => 19,
+        't' => 20,
+        'u' => 21,
+        'v' => 22,
+        'w' => 23,
+        'x' => 24,
+        'y' => 25,
+        'z' => 26,
+        'A' => 27,
+        'B' => 28,
+        'C' => 29,
+        'D' => 30,
+        'E' => 31,
+        'F' => 32,
+        'G' => 33,
+        'H' => 34,
+        'I' => 35,
+        'J' => 36,
+        'K' => 37,
+        'L' => 38,
+        'M' => 39,
+        'N' => 40,
+        'O' => 41,
+        'P' => 42,
+        'Q' => 43,
+        'R' => 44,
+        'S' => 45,
+        'T' => 46,
+        'U' => 47,
+        'V' => 48,
+        'W' => 49,
+        'X' => 50,
+        'Y' => 51,
+        'Z' => 52,
+        _ => 0,
+    }
+}
